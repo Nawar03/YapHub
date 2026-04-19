9f9eb87376fe096fe56591d9794083a4365de210
Oskar Krzyzak <oskar.krzyzak.ok@gmail.com>
2026-04-19 18:08:15 -0500

    Fix: clickable nicknames in comments on all feed pages

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/images', express.static('images'));
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Elmo123!',
+  password: 'marwip777',
   database: 'yaphub'
 });
 
@@ -501,7 +501,7 @@ app.get('/posts', (req, res) => {
 app.get('/posts/:post_id/comments', (req, res) => {
   const postId = req.params.post_id;
   const sql = `
-  SELECT comments.comment_id, comments.content, comments.created_at, users.nickname
+  SELECT comments.comment_id, comments.content, comments.created_at, users.nickname, comments.user_id
   FROM comments
   JOIN users ON comments.user_id = users.user_id
   WHERE comments.post_id = ?
